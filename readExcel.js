97f2b24179eeed21e1f70d575244e61551bc32ab
Steven Yu <stevenyuysy@gmail.com>
2017-05-28 16:08:04 +0800

    fix: support 500 columns

--- readExcel.js
+++ readExcel.js
@@ -45,7 +45,7 @@ function readIn(book) {
 
   for (let z in worksheet) {
     if (z[0] === '!') continue;
-    if (parseInt(z.substring(1)) > 6) {
+    if (z.slice(reg.exec(z).index) > 6) {
       let column_number = excel_num.letter_to_number[(z.slice(0, reg.exec(z).index))]
       if (column_number === -1) {
         coordinateX.push(parseFloat(worksheet[z].v));
@@ -54,8 +54,8 @@ function readIn(book) {
       }
     }
   }
-  // console.log(coordinateX);
-  // console.log(coordinateY);
+  console.log(coordinateX);
+  console.log(coordinateY);
 
   for (let j in coordinateY) {
     for (let k in coordinateX) {
@@ -97,15 +97,5 @@ function readIn(book) {
 
   return render_data;
 }
-
-/**
- * 用于转换大写字母到自然数，映射关系为 {A-Z} -> {1-26}
- * @param {String} s - 用于转换的大写字母
- * @return {Number} 转换后的自然数
- */
-function c2n(s) {
-  return s.charCodeAt(0) - 64
-}
-
 
 module.exports = readIn;
